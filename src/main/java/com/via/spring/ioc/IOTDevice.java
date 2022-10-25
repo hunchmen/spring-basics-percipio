9cd8fac6c05eb8a98f3ce8b74a06e875da63e600
hunchmen <valalberto0214@gmail.com>
2022-10-25 12:10:47 +0800

    course2: creating a bean configuration

---
+++ src/main/java/com/via/spring/ioc/IOTDevice.java
@@ -0,0 +1,42 @@
+/**
+ * Licensed Materials - Property of hunchmen
+ * 
+ * (C) hunchmen. 2022. All Rights Reserved.
+ * 
+ */
+package com.via.spring.ioc;
+
+/**
+ * 
+ * @author via
+ * 
+ * @date 25 Oct 2022
+ */
+public class IOTDevice {
+
+    String device = "Air Conditioner";
+    String description = "Temperature sensor";
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "IOTDevice {" + "device = '" + device + "!\'"
+                + ", description = '" + description + "\'" + '}';
+    }
+
+}
